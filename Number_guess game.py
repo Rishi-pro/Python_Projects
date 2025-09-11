77ab586b0ee42f318b4c60c58b4d8cf78a5c2bf4
Rishi-pro <mawalr@students.hcboe.net>
2025-09-11 18:29:54 -0400

    Update Number_guess game.py

--- Number_guess game.py
+++ Number_guess game.py
@@ -1,1 +1,12 @@
+#this would ask python for a random thing
+import random
+#secret would be the number computer would import from the random
+secret= random.randint(1,10)
+# ask user to guess the number
 
+guess= int(input('guess the number from between 1 to 10: '))
+#this creates a situiation if user does guess or does not guess
+if guess==secret:
+    print('woah! you guessed the number')
+else:
+    print('you did not guess the number/nthe correct number was',secret)
