3c51190c567d7ef7b501e91a4dbfa9d8a7aa4603
Rishi-pro <mawalr@students.hcboe.net>
2025-09-13 10:52:00 -0400

    Update number_guessing_game_upgraded.py

--- number_guessing_game_upgraded.py
+++ number_guessing_game_upgraded.py
@@ -1,6 +1,5 @@
 #SO THIS IS GOING TO BE THE NEXT VERSION OF THE GUESS GAME
 import random 
-
 secret= random.randint(1,20)
 chances= 3 #so this would be the major change in this program
 while chances>0:
